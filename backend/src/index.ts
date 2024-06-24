39f3c00f72c4a54a8cbeb70b150942eebc538e7f
Rajan <rajanj448@student.sfit.ac.in>
2024-06-24 17:17:00 +0530

    Register completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import "dotenv/config";
 import path from 'path';
 import mongoose from 'mongoose';
+import userRoutes from "./routes/users";
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
@@ -14,6 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+
+
+app.use("/api/users",userRoutes)
 
 app.get("*", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
